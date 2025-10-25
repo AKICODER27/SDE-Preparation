0f9b815ecae7049e8bd518fd64a76d253f59d52b
Akshat Dholakia <akshatdholakia7@gmail.com>
2025-10-26 00:32:01 +0530

    Add files via upload

---
+++ conditions.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num=10;
+    if(num>20)
+    {
+        cout<<"LOL";
+
+    }
+    else
+    {
+        cout<<"HURRAY";
+    }
+
+    int num=21;
+    if(num%2==0)
+    cout<<"EVEN NUMBER";
+    else
+    cout<<"Odd number";
+    return 0;
+
+}
